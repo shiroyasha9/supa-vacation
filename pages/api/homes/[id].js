5b3272f6cad5058e8294760d5e6bf7aabd118cf2
Mubin Ansari <mubinansari910@gmail.com>
2023-01-20 20:37:50 +0530

    add functionality to edit a place

---
+++ pages/api/homes/[id].js
@@ -0,0 +1,47 @@
+import { PrismaClient } from '@prisma/client';
+import { getSession } from 'next-auth/react';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  const session = await getSession({ req });
+  console.log('hi');
+
+  if (!session) {
+    return res.status(401).json({ message: 'Unauthorized.' });
+  }
+
+  // Retrieve the authenticated user
+  const user = await prisma.user.findUnique({
+    where: { email: session.user.email },
+    select: { listedHomes: true }
+  });
+
+  console.log(user);
+
+  // Check if authenticated user is the owner of this home
+  const { id } = req.query;
+  if (!user?.listedHomes?.find(home => home.id === id)) {
+    return res.status(401).json({ message: 'Unauthorized.' });
+  }
+
+  // Update home
+  if (req.method === 'PATCH') {
+    try {
+      const home = await prisma.home.update({
+        where: { id },
+        data: req.body
+      });
+      res.status(200).json(home);
+    } catch (e) {
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  }
+  // HTTP method not supported!
+  else {
+    res.setHeader('Allow', ['PATCH']);
+    res
+      .status(405)
+      .json({ message: `HTTP method ${req.method} is not supported.` });
+  }
+}
