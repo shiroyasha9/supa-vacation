23f87eb6432130587af4d11e1b2aaf1cd785a31d
Mubin Ansari <mubinansari910@gmail.com>
2023-01-20 18:16:18 +0530

    fetch the home data

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
-import Layout from '@/components/Layout';
 import Grid from '@/components/Grid';
+import Layout from '@/components/Layout';
+import { PrismaClient } from '@prisma/client';
 
-import homes from 'data.json';
+const prisma = new PrismaClient();
 
-export default function Home() {
+export async function getServerSideProps() {
+  // Get all homes
+  const homes = await prisma.home.findMany();
+  // Pass the data to the Home page
+  return {
+    props: {
+      homes: JSON.parse(JSON.stringify(homes))
+    }
+  };
+}
+
+export default function Home({ homes = [] }) {
   return (
     <Layout>
-      <h1 className="text-xl font-medium text-gray-800">
+      <h1 className='text-xl font-medium text-gray-800'>
         Top-rated places to stay
       </h1>
-      <p className="text-gray-500">
+      <p className='text-gray-500'>
         Explore some of the best places in the world
       </p>
-      <div className="mt-8">
+      <div className='mt-8'>
         <Grid homes={homes} />
       </div>
     </Layout>
